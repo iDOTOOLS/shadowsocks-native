7ddf7e8bb5ee40ece99dee0d8d75de0cf91cbf43
LvWind <lvwind.shaw@gmail.com>
2016-06-22 18:03:38 +0800

    change ss-local timeout

--- jni/shadowsocks-libev/src/jconf.h
+++ jni/shadowsocks-libev/src/jconf.h
@@ -26,7 +26,7 @@
 #define MAX_REMOTE_NUM 10
 #define MAX_CONF_SIZE 128 * 1024
 #define MAX_DNS_NUM 4
-#define MAX_CONNECT_TIMEOUT 10
+#define MAX_CONNECT_TIMEOUT 120 //modify timeout
 #define MIN_UDP_TIMEOUT 10
 
 typedef struct {
